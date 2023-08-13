28fd4376d2573dc0783881c3a7f5f189ee3c3359
kaqueDM1 <kaiquedemetrio2017123@gmail.com>
2023-08-13 13:37:35 -0300

    2.0

--- exercicio1.py
+++ exercicio1.py
@@ -1,3 +1,6 @@
+#Crie uma lista que simule a contagem regressiva de um foguete sendo enviado para fora da Terra.
+#Após isso responda, é uma pilha ou uma fila;
+
 lista = []
 for i in range(11):
     lista.append(i)
